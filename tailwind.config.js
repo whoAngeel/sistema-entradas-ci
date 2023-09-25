816ce953fb7d66f55fbb032876c5ba64c5610f48
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-09-24 21:53:50 -0600

    feat(tailwind): instalando tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:['./src/renderer/index.html','./src/renderer/src/**/*.{vue,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
